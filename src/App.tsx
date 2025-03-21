c7695674e1302d29d409f25ffd879d8dc2203a81
Pascal-Juma <pascaljuma2002@gmail.com>
2025-03-21 12:55:58 +0300

    Set a version Control for the Mut Tech Club

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Hero from './assets/pages/Home/Hero/Hero'
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/pages/Home/Hero/Hero.tsx
@@ -0,0 +1,48 @@
+import './Hero.css'
+
+function Hero() {
+    return (
+        <>
+            <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" data-bs-interval="1000" data-bs-wrap="true">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner" data-bs-interval="1000">
+                    <div className="carousel-item active" data-bs-interval="1000">
+                        <img src="/src/assets/images/hero-a.jpg" className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item" data-bs-interval="1000">
+                        <img src="/src/assets/images/hero-b.jpg" className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item" data-bs-interval="1000">
+                        <img src="/src/assets/images/hero-c.jpg" className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Hero
